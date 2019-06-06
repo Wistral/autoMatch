a45bc5fb624cc6ad34e2107ca340b4da629e26f9
Wistral <whistral@gmail.com>
2019-06-06 15:50:07 +0800

    improve cmd path adaptivity

--- full-match.sh
+++ full-match.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
-if [ $# -lt 2 ];
+if [[ $# -lt 2 ]];
 then
 echo "usage:
-$0 team1folder team2folder <serverHost>=localhost"
+$0 team1folder team2folder <serverHost>=localhost identification"
 exit 1
 fi
 halfTime=355
 
 CUR_DIR="$( cd "$( dirname "$0" )" && pwd )"
+TEAM1_DIR="$( cd "$( dirname "$1" )" && pwd )"
+TEAM2_DIR="$( cd "$( dirname "$2" )" && pwd )"
+
 # start rcssserver3d and block output
 startServer()
 {
@@ -18,31 +21,31 @@ startServer()
 
 halfMatch()
 {
-    cd $CUR_DIR
+    cd ${CUR_DIR}
     startServer
     sleep 3
-    cd $1
+    cd ${TEAM1_DIR}
     ./start.sh $3
     sleep 3
-    cd $CUR_DIR
-    cd $2
+    cd ${CUR_DIR}
+    cd ${TEAM2_DIR}
     ./start.sh $3
     sleep 3
-    cd $CUR_DIR
+    cd ${CUR_DIR}
     # kick off
     perl kickoff.pl
     
-    PLAYMODE=""
+    PLAY_MODE=""
     # half match completed
-    while [ ! "$PLAYMODE" = "BeforeKickOff" ]
+    while [[ ! "$PLAY_MODE" = "BeforeKickOff" ]]
     do
       sleep 5
-      PLAYMODE=$(./infoParser.py sparkmonitor.log playmode)
+      PLAY_MODE=$(./infoParser.py sparkmonitor.log playmode)
     done
     # kill all agents
     cd $1
     ./kill.sh
-    cd $CUR_DIR
+    cd ${CUR_DIR}
     cd $2
     ./kill.sh
     killall -9 rcssserver3d
@@ -51,7 +54,7 @@ halfMatch()
 
 #source match-script.sh
 # set server host
-if [ "$3" = "" ]
+if [[ "$3" = "" ]]
 then
 serverHost='localhost'
 else
@@ -60,8 +63,8 @@ fi
 
 #startServer
 # start match and save log file
-halfMatch $1 $2 $serverHost
-mv $CUR_DIR/sparkmonitor.log $CUR_DIR/$1-vs-$2-first-half$4.log
+halfMatch $1 $2 ${serverHost}
+mv ${CUR_DIR}/sparkmonitor.log ${CUR_DIR}/$1-vs-$2-first-half$4.log
 
-halfMatch $2 $1 $serverHost
-mv $CUR_DIR/sparkmonitor.log $CUR_DIR/$1-vs-$2-second-half$4.log
+halfMatch $2 $1 ${serverHost}
+mv ${CUR_DIR}/sparkmonitor.log ${CUR_DIR}/$1-vs-$2-second-half$4.log
