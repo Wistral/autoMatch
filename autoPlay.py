ddcc5f92e8e5bc78c9b0fa5c8db5d343fb13d83d
ZonePG <619586533.com>
2019-06-07 10:09:01 -0400

    fix uuid format error

--- autoPlay.py
+++ autoPlay.py
@@ -8,12 +8,16 @@ from display import db, history
 if __name__ == '__main__':
     print('connect to SQL server successfully!')
     ourTeam = 'HfutEngine2019'
+    codeDir = '/home/robocup3d/spark/2019/'
     # oppo_teams = []
     # get team names
-    oppo_teams = os.popen('ls -d */')
-    oppo_teams = [_[:-2] for _ in oppo_teams]
-    oppo_teams.remove('__pycache__')
-    oppo_teams.remove('HfutEngine2019')
+    oppo_teams = os.popen('ls -d {}*/'.format(codeDir))
+    oppo_teams = [_.strip('/\n')[len(codeDir)-1:] for _ in oppo_teams]
+    print(oppo_teams)
+    if '__pycache__' in oppo_teams:
+        oppo_teams.remove('__pycache__')
+    if ourTeam in oppo_teams:
+        oppo_teams.remove(ourTeam)
     random.shuffle(oppo_teams)
     serverHost = 'localhost'
 
@@ -22,18 +26,19 @@ def match():
     for oppo in oppo_teams:
         print('==============================================')
         print('running task: ' + ourTeam + ' vs ' + oppo + '......')
-        uuid = os.popen('cat /proc/sys/kernel/random/uuid')
-        os.system('./full-match.sh {} {} {} {} >/dev/null 2>&1'.format(ourTeam, oppo, serverHost, uuid))
-        first_half_scores, second_half_scores = \
-            getInfo(uuid+'.log'.format(ourTeam, oppo, uuid), 'score'),\
-            getInfo(uuid+'.log'.format(ourTeam, oppo, uuid), 'score')
-        left_score, right_score = first_half_scores[0] + second_half_scores[1], \
-                                  first_half_scores[1] + second_half_scores[0]
-        history.execute(
-            """ insert into `matchHistory`values (now(),'{}',{},{});""".format(oppo, left_score, right_score, uuid))
-        print('Finished, result is {} : {}'.format(left_score, right_score))
-        db.commit()
-        print('Match history updated!')
+        for uuid in os.popen('cat /proc/sys/kernel/random/uuid'):
+            uuid = uuid[:-1]
+            os.system('./full-match.sh {} {} {} {} {}'.format(codeDir, ourTeam, oppo, serverHost, uuid))
+            first_half_scores, second_half_scores = \
+                getInfo(uuid+'.log'.format(ourTeam, oppo, uuid), 'score'),\
+                getInfo(uuid+'.log'.format(ourTeam, oppo, uuid), 'score')
+            left_score, right_score = first_half_scores[0] + second_half_scores[1], \
+                                      first_half_scores[1] + second_half_scores[0]
+            history.execute(
+                """ insert into `matchHistory`values (now(),{},{},'{}');""".format(oppo, left_score, right_score, uuid))
+            print('Finished, result is {} : {}'.format(left_score, right_score))
+            db.commit()
+            print('Match history updated!')
 
 
 if __name__ == '__main__':
